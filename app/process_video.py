8ff4daaa57cac8ae13140b2daaa3055dd37e57a4
Prince Gupta <princegupta1586@gmail.com>
2024-07-15 01:02:26 +0530

    Check video if 9:16

---
+++ app/process_video.py
@@ -0,0 +1,15 @@
+from utils.is_16_9_ratio import is_16_9_ratio
+from moviepy.editor import VideoFileClip
+from moviepy.video.fx.all import resize, margin
+import os
+
+def process_video(video_path):
+    video = VideoFileClip(video_path)
+    # Resize and add black borders if necessary
+    if not is_16_9_ratio(video.size):
+        video = resize(video, height=1080)
+        video = margin(video, top=(1080 - video.size[1]) // 2, bottom=(1080 - video.size[1]) // 2,
+                       left=(1920 - video.size[0]) // 2, right=(1920 - video.size[0]) // 2, color=(0, 0, 0))
+    processed_video_path = os.path.join("C:/Users/princ/Desktop/tempFiles", "processed_video.mp4")
+    video.write_videofile(processed_video_path, codec='libx264')
+    return processed_video_path
