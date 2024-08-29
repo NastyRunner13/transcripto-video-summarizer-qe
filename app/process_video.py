1f45d6b329847275465616071f2bc6c2e0a74a3c
Prince Gupta <princegupta1586@gmail.com>
2024-08-30 00:48:15 +0530

    Fix broken pipeline issue

--- app/process_video.py
+++ app/process_video.py
@@ -4,12 +4,19 @@ from moviepy.video.fx.all import resize, margin
 import os
 
 def process_video(video_path, upload_dir):
-    video = VideoFileClip(f"file:{video_path}")
+    # Ensure the file path is correctly formatted for MoviePy/FFmpeg
+    video_path = os.path.abspath(video_path)  # Get the absolute path
+    video_path = f"file:///{video_path.replace('\\', '/')}"  # Convert to URI format
+
+    video = VideoFileClip(video_path)
+
     # Resize and add black borders if necessary
     if not is_16_9_ratio(video.size):
         video = resize(video, height=1080)
         video = margin(video, top=(1080 - video.size[1]) // 2, bottom=(1080 - video.size[1]) // 2,
                        left=(1920 - video.size[0]) // 2, right=(1920 - video.size[0]) // 2, color=(0, 0, 0))
+
     processed_video_path = os.path.join(upload_dir, "processed_video.mp4")
     video.write_videofile(processed_video_path, codec='libx264')
+    
     return processed_video_path
