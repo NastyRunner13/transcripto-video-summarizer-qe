e59e68d34d7a0458eb315e600d7bbffef8c48f4c
Prince Gupta <princegupta1586@gmail.com>
2024-08-30 00:37:36 +0530

    Change function

--- utils/is_16_9_ratio.py
+++ utils/is_16_9_ratio.py
@@ -2,7 +2,7 @@ import math
 
 def is_16_9_ratio(size):
     width, height = size
-    gcd = math.gcd(width, height)
-    reduced_width = width // gcd
-    reduced_height = height // gcd
+    gcd = math.gcd(width, height)  # Calculate the greatest common divisor
+    reduced_width = width // gcd   # Reduce width by dividing by GCD
+    reduced_height = height // gcd # Reduce height by dividing by GCD
     return (reduced_width, reduced_height) == (16, 9)
